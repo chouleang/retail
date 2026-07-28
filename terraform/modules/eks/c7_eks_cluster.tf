379ae2529c9a75219120bd223f8bf39c34c535f6
sq <sq@sqs-MacBook-Air.local>
2026-07-28 11:51:19 +0700

    eks correction

--- terraform/modules/eks/c7_eks_cluster.tf
+++ terraform/modules/eks/c7_eks_cluster.tf
@@ -15,7 +15,7 @@ resource "aws_eks_cluster" "main" {
   # VPC configuration for control plane networking
   vpc_config {
     # Subnets where EKS control plane ENIs will be placed (should be private)
-    subnet_ids = data.terraform_remote_state.vpc.outputs.private_subnet_ids
+    subnet_ids = var.private_subnet_ids
 
     # Allow access to private endpoint (inside VPC)
     endpoint_private_access = var.cluster_endpoint_private_access
--- terraform/modules/eks/c9_eks_nodegroup_private.tf
+++ terraform/modules/eks/c9_eks_nodegroup_private.tf
@@ -11,7 +11,7 @@ resource "aws_eks_node_group" "private_nodes" {
   node_role_arn = aws_iam_role.eks_nodegroup_role.arn
 
   # Subnets where the worker nodes will be launched (typically private subnets)
-  subnet_ids = data.terraform_remote_state.vpc.outputs.private_subnet_ids
+  subnet_ids = var.private_subnet_ids
 
   # Instance types for the nodes (e.g., t3.medium, m5.large)
   instance_types = var.node_instance_types
